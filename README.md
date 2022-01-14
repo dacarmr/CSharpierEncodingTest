a400426c0b1ac0756c37be3289fe1552c9c23fed
Daniel Carman <dacarm@rinna.co.jp>
2022-01-14 14:53:23 +0900

    Sample code initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Encoding Test For CSharpier
+
+Currently for me using vs2022 + extension to format on save the encoding of Japanese characters is lost.
+This demo repo is used as a sample to address this issue.
---
+++ source/CSharpierEncodingTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+const char test = '。';
+const string test1 = "こにちは、世界！";
+var encoding = System.Text.Encoding.Default;
+Console.OutputEncoding = encoding;
+Console.InputEncoding = encoding;
+
+// Outputting to console may/may not work correctly. At least in my environment I have to change the font to MSMincho or MSGothic.
+Console.WriteLine(test);
+// This line should output correctly.
+Debug.WriteLine($"{test} {test1}");
